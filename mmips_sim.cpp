a1022dfad46c4605944b94998f02e144318ae9a9
Michiel Visser <info@webmichiel.nl>
2018-02-15 16:34:24 +0100

    Fixed RAM timings

--- mmips_sim.cpp
+++ mmips_sim.cpp
@@ -48,6 +48,41 @@ unsigned int img[] = {
     0x1010100f, 0x0f0f0f10, 0x0f10100f, 0x0f101010, 0x1011110f, 0x110f142f, 0x3b3a3d3b, 0x424e5e74,
 };
 
+unsigned int ref_out[] = {
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+    0x00161716, 0x17161716, 0x18161715, 0x18171817, 0x19171800, 0x3e3d3e39, 0x443f4127, 0x65616300,
+    0x00171817, 0x17161716, 0x17161716, 0x19171817, 0x18171803, 0x4e373b34, 0x433d4223, 0x775f6600,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x18171802, 0x4d373b34, 0x423a3f21, 0x7e636b00,
+    0x00161716, 0x17161717, 0x17161716, 0x19171817, 0x17161701, 0x4d373b35, 0x433c4124, 0x82636b00,
+    0x00161716, 0x18171817, 0x17161716, 0x19171818, 0x18171803, 0x4e383c34, 0x423b4023, 0x80626a00,
+    0x00161716, 0x18161716, 0x17161716, 0x19171818, 0x16151600, 0x4b353931, 0x433c4123, 0x7f626a00,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x16161703, 0x4f393d35, 0x463e4325, 0x83666e00,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x17161701, 0x4b343831, 0x443c4122, 0x82646c00,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x17161702, 0x4d393c33, 0x443c4124, 0x83656d00,
+    0x00161717, 0x17161716, 0x17161716, 0x19171817, 0x17161703, 0x4b343830, 0x443d4224, 0x80626a00,
+    0x00171817, 0x17161716, 0x17161716, 0x19171817, 0x18171805, 0x4935392f, 0x443b4021, 0x80646c00,
+    0x00161716, 0x17161716, 0x17161716, 0x19171818, 0x17161702, 0x4632362f, 0x443b4020, 0x82646c00,
+    0x00161716, 0x17161717, 0x17161715, 0x18171817, 0x17161704, 0x4a363a2f, 0x443b4022, 0x85666e00,
+    0x00161716, 0x17161716, 0x16151614, 0x19171817, 0x17161703, 0x4732362e, 0x473e4324, 0x84656d00,
+    0x00161717, 0x17161716, 0x17161716, 0x19171817, 0x17161704, 0x4935392f, 0x443a3f20, 0x82656d00,
+    0x00161716, 0x16151615, 0x17161716, 0x19171817, 0x17161703, 0x4632352b, 0x433b4023, 0x84646c00,
+    0x00161716, 0x17161716, 0x18161716, 0x19171817, 0x17161703, 0x4835392f, 0x483e4325, 0x82646c00,
+    0x00161717, 0x18161716, 0x17161716, 0x19171816, 0x16161705, 0x4934382d, 0x453c4121, 0x81656d00,
+    0x00171817, 0x16151615, 0x17161716, 0x18161716, 0x18171807, 0x4632362a, 0x463d4227, 0x87687100,
+    0x00161716, 0x16161716, 0x18161715, 0x18171818, 0x18161703, 0x4230342b, 0x483d4229, 0x7f5f6800,
+    0x00161717, 0x17161716, 0x17161716, 0x19171817, 0x17161704, 0x4634382e, 0x473b4029, 0x765b6300,
+    0x00161716, 0x16151615, 0x16151615, 0x18161717, 0x17161703, 0x4632362c, 0x453c412b, 0x72596000,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x16151604, 0x4633372a, 0x463c402d, 0x6f575e00,
+    0x00171817, 0x17161716, 0x17161716, 0x19171817, 0x17171806, 0x4632362b, 0x4a40452c, 0x6b565d00,
+    0x00161715, 0x17161717, 0x17161716, 0x19171818, 0x17161704, 0x4331352b, 0x473b4024, 0x6f5c6300,
+    0x00161716, 0x18171818, 0x17161715, 0x18171817, 0x17161705, 0x4633372c, 0x473d4225, 0x785f6600,
+    0x00171817, 0x19171817, 0x17161715, 0x1a181918, 0x18171807, 0x45313528, 0x473d4225, 0x7d636a00,
+    0x00161716, 0x17161716, 0x17161716, 0x19171817, 0x17161707, 0x412f3326, 0x483d4224, 0x7e626a00,
+    0x00171817, 0x17161717, 0x17161715, 0x18171817, 0x18171806, 0x3f2e3226, 0x4a3e4322, 0x82666e00,
+    0x00161715, 0x17161717, 0x16151614, 0x19171818, 0x17161704, 0x4231352b, 0x4c3e431f, 0x86697100,
+    0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+};
+
 const char *reg_names[] = {
     "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
     "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
@@ -72,10 +107,13 @@ int main(int argc, char **argv, char **env) {
     ram[2] = ram[3] = ram[4] = ram[5] = 0x00400000;
 
     Vmmips* mmips = new Vmmips;
+
+#ifdef TRACE
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     mmips->trace(tfp, 99);
-    tfp->open("simx.vcd");
+    tfp->open("../mmips_trace.vcd");
+#endif
 
     printf("Enable mMIPS\n");
     mmips->enable = 1;
@@ -83,113 +121,131 @@ int main(int argc, char **argv, char **env) {
 
     int rom_addr = 0x00000000;
     int rom_r = 0;
-    int ram_addr = 0x00000000;
-    int ram_din = 0x00000000;
-    int ram_r = 0;
-    int ram_w = 0;
 
     printf("Reset mMIPS\n");
     mmips->rst = 1;
     mmips->clk = 0;
     mmips->eval();
+#ifdef TRACE
+    tfp->dump(0);
+#endif
+
     rom_addr = mmips->rom_addr;
     rom_r = mmips->rom_r;
     mmips->clk = 1;
     mmips->eval();
-    tfp->dump(0);
+#ifdef TRACE
+    tfp->dump(50);
+#endif
+
     mmips->rst = 0;
     mmips->clk = 0;
-    if (rom_r) {
-        printf("rom read at %.8x\n", rom_addr);
-        mmips->rom_dout = bswap(rom[rom_addr >> 2]);
-    }
+    if (rom_r) mmips->rom_dout = bswap(rom[rom_addr >> 2]);
     mmips->eval();
-    tfp->dump(50);
+#ifdef TRACE
+    tfp->dump(100);
+#endif
 
+    int ram_addr = 0;
+    int ram_lblw = 0;
+    int ram_r = 0;
+    int ram_w = 0;
+    int ram_byteselect_before = 0;
+    int ram_byteselect = 0;
+    int ram_din = 0;
+    int ram_we = 0;
+
+    printf("Running mMIPS\n");
     int clock = 0;
-    while (clock < 80 || mmips->mMIPS__DOT__bus_pc != 0x44) {
-        // if (false && mmips->mMIPS__DOT__bus_pc == 0x364) {
-        //     printf("\n---- BREAKPOINT ----\n");
-        //     for (int r = 0; r < 4; r++) {
-        //         for (int c = 0; c < 8; c++) {
-        //             printf("%8s ", reg_names[r*8+c]);
-        //         }
-        //         printf("\n");
-        //         for (int c = 0; c < 8; c++) {
-        //             printf("%.8x ", mmips->mMIPS__DOT__registers__DOT__regs[r*8+c]);
-        //         }
-        //         printf("\n");
-        //     }
-        //     printf("--------------------\n");
-
-        //     getchar();
-        // }
-
-        printf(">> %d: %s\n", ++clock, mmips->mMIPS__DOT__bus_hazard_hazard ? "hazard" : "");
+    while (mmips->mMIPS__DOT__bus_pc != 0x44) {
+        printf("\r>> %9d cycles", ++clock);
 
         /* Rising edge */
         rom_addr = mmips->rom_addr;
         rom_r = mmips->rom_r;
 
-        ram_addr = mmips->ram_addr;
+        ram_addr = mmips->ram_addr & 0x3fffff;
+        if (ram_r == 0b10) {
+            ram_lblw = 1;
+        } else {
+            ram_lblw = 0;
+        }
         ram_r = mmips->ram_r;
         ram_w = mmips->ram_w;
-        ram_din = mmips->ram_din;
+        ram_byteselect = ram_byteselect_before;
+
+        if (ram_addr < RAMSIZE) {
+            ram_byteselect_before = ram_addr & 0b11;
+            ram_din = mmips->ram_din;
+            if (ram_w == 0b01) {
+                ram_we = 0x00000000;
+            } else if (ram_w == 0b10) {
+                unsigned int ram_we_table[4] = {0x00ffffff, 0xff00ffff, 0xffff00ff, 0xffffff00};
+                ram_we = ram_we_table[ram_byteselect_before];
+                ram_din = (ram_din & 0xff) << ((3 - ram_byteselect_before) * 8);
+            } else {
+                ram_we = 0xffffffff;
+            }
+        }
+
+        if ((ram_r || ram_w) && ram_addr < RAMSIZE) {
+            unsigned int value = ram[ram_addr >> 2];
+            if (ram_lblw) {
+                mmips->ram_dout = (value >> ((3 - ram_byteselect) * 8)) & 0xff;
+            } else {
+                mmips->ram_dout = value;
+            }
+        } else {
+            mmips->ram_dout = 0;
+        }
 
         mmips->clk = 1;
         mmips->eval();
-        tfp->dump(clock*100);
+#ifdef TRACE
+        tfp->dump(clock*100 + 50);
+#endif
 
         /* Falling edge */
         mmips->clk = 0;
         if (rom_r) {
-            printf("rom read at %.8x\n", rom_addr);
             assert(rom_addr < ROMSIZE);
             mmips->rom_dout = bswap(rom[rom_addr >> 2]);
         }
-        unsigned int ram_offset = ram_addr & 0x3fffff;
-        if (ram_offset < RAMSIZE) {
-            if (ram_r == 1) {
-                mmips->ram_dout = ram[ram_offset >> 2];
-                printf("ram read word %.8x at %.8x\n", mmips->ram_dout, ram_addr);
-            } else if (ram_r == 2) {
-                mmips->ram_dout = ram[ram_offset >> 2];
-                // mmips->ram_dout = *(((unsigned char *) ram) + ram_offset);
-                printf("ram read byte %.2x at %.8x\n", mmips->ram_dout, ram_addr);
-            }
 
-            if (ram_w == 1) {
-                printf("ram write word %.8x at %.8x\n", ram_din, ram_addr);
-                ram[ram_offset >> 2] = ram_din;
-            } else if (ram_w == 2) {
-                printf("ram write byte %.2hhx at %.8x\n", (unsigned char) ram_din, ram_addr);
-                *(((unsigned char *) ram) + ram_offset) = ram_din;
-            }
-        } else if (ram_r) {
-            printf(">>>> read at invalid addr: %.8x\n", ram_addr);
-        } else if (ram_w) {
-            printf(">>>> write at invalid addr: %.8x\n", ram_addr);
+        if (ram_addr < RAMSIZE) {
+            ram[ram_addr >> 2] = (ram[ram_addr >> 2] & ram_we) | (ram_din & ~ram_we);
         }
 
         mmips->eval();
-        tfp->dump(clock*100 + 50);
+#ifdef TRACE
+        tfp->dump(clock*100 + 100);
+#endif
     }
 
-    printf("Finished\n");
+    printf("\nFinished\n");
 
     for (int i = 0; i < 256; i++) {
         unsigned int value = ram[(0x2000 >> 2) + i];
         for (int j = 0; j < 4; j++) {
-            unsigned char b = (value >> (j*8)) & 0xff;
+            unsigned char b = (value >> ((3-j)*8)) & 0xff;
             char color[64] = {0};
             snprintf(color, 64, "\x1b[48;2;%hhu;%hhu;%hhum\x1b[38;2;%hhu;%hhu;%hhum", b, b, b, b, b, b);
             printf("%s%.2x", color, b);
         }
         if (i % 8 == 7) printf("\x1b[0m\n");
     }
-    printf("Clean\n");
+    for (int i = 0; i < 256; i++) {
+        unsigned int value = ram[(0x2000 >> 2) + i];
+        unsigned int ref_value = ref_out[i];
+        if (value != ref_value) {
+            printf("Output mismatch at %.6x, expected %.8x got %.8x\n", 0x402000 + i, ref_value, value);
+        }
+    }
 
+#ifdef TRACE
     tfp->close();
+    delete tfp;
+#endif
     delete mmips;
     free(rom);
     free(ram);
